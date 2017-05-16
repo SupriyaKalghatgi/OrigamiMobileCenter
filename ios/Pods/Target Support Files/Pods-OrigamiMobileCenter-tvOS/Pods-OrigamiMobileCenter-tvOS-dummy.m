029140903cbab879a11b0a2ee5d6dc512eeb72a8
Supriya <supriya@Supriyas-MacBook-Air.local>
2017-05-16 17:46:15 +0530

    configured mobilecenter

---
+++ ios/Pods/Target Support Files/Pods-OrigamiMobileCenter-tvOS/Pods-OrigamiMobileCenter-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OrigamiMobileCenter_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_OrigamiMobileCenter_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-OrigamiMobileCenter/Pods-OrigamiMobileCenter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_OrigamiMobileCenter : NSObject
+@end
+@implementation PodsDummy_Pods_OrigamiMobileCenter
+@end
