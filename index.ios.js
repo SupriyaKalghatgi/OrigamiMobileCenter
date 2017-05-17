430da6e277a560759f6a76c0408c6bfb2fded079
Supriya <supriya@Supriyas-MacBook-Air.local>
2017-05-17 18:10:52 +0530

    Running on iOS device

--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,10 @@ export default class OrigamiMobileCenter extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          GeekyAnts
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Origami
         </Text>
       </View>
     );
